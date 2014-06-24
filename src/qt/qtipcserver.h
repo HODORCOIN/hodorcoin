eb5b2a1fc9c07e20ef962aa4139a810bf42c6137
Michael Shihinski <GraphicTEN2@Michaels-Mac-Pro.local>
2014-06-24 16:07:44 -0400

    initial commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define HODORCOIN-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "HODORCOINURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
